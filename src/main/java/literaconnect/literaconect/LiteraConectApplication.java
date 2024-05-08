81a2a7f69cb51f088d0dbe4f60a05383604269ed
Ariadne Cavilha <ariadne.jorge@teclogica.com>
2024-05-07 23:20:01 -0300

    feat: Modificando para projeto spring

---
+++ src/main/java/literaconnect/literaconect/LiteraConectApplication.java
@@ -0,0 +1,13 @@
+package literaconnect.literaconect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraConectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraConectApplication.class, args);
+	}
+
+}
